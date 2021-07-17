c4837c3884f246f379274e3e81dccce53d778932
ankit <ankitforstudy@gmail.com>
2021-07-17 19:22:16 +0530

    MockMvc - with MockBean example

--- src/test/java/com/example/demo/ControllerTestUserMockBeanRepoMocking.java
+++ src/test/java/com/example/demo/ControllerTestUserMockBeanRepoMocking.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
 import com.example.demo.controller.ApplicationController;
+import com.example.demo.exception.ServiceException;
 import com.example.demo.proxy.FeignClient;
 import com.example.demo.repo.User;
 import com.example.demo.repo.UserRepository;
 import com.example.demo.service.Service;
+import com.example.demo.service.ServiceImpl;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,7 +16,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -26,6 +30,17 @@ import java.util.Optional;
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class ControllerTestUserMockBeanRepoMocking {
+
+    @org.springframework.boot.test.context.TestConfiguration
+    static class TestConfiguration{
+        @Bean
+        public Service getService(){
+            return new ServiceImpl();
+        }
+    }
+
+    @Autowired
+    private Service service;
 
     @Autowired
     private TestEntityManager testEntityManager;
